d4024aa690bfdac25bbb84b7b66837420d83c053
woojoung <ywj981217@naver.com>
2021-08-10 03:03:41 +0900

    test1

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("hello world");
+import getType from './getType'
+
+
+console.log(typeof "hello world");
+
+console.log(typeof 150) ;
+
+console.log(typeof true) 
+
+console.log(typeof {})
+
+console.log(typeof [])
+
+
+
+console.log(getType(123))
+console.log(getType(false))
+console.log(getType([1,2,3]))
