95ac7e4d5f05a4fff39c9b6640536ee704d7ba5d
woojoung <ywj981217@naver.com>
2021-08-11 01:14:01 +0900

    js practice

--- main.js
+++ main.js
@@ -1,18 +1,14 @@
-import getType from './getType'
+const ulEl = document.querySelector('ul');
 
+console.log(ulEl);
 
-console.log(typeof "hello world");
-
-console.log(typeof 150) ;
-
-console.log(typeof true) 
-
-console.log(typeof {})
-
-console.log(typeof [])
-
-
-
-console.log(getType(123))
-console.log(getType(false))
-console.log(getType([1,2,3]))
+for (let i=0; i<3; i+=1) {
+  const li = document.createElement('li')
+  li.textContent = `list-${i+1}`
+  if ((i+1)%2 ===0) {
+    li.addEventListener('click',function () {
+      console.log(li.textContent)
+    })
+  }
+  ulEl.appendChild(li)
+}
