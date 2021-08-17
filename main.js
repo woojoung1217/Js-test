344a97412f0774d1d649eb87ced705eac6d14dc5
woojoung <ywj981217@naver.com>
2021-08-17 15:14:03 +0900

    new js<생성자 함수>

--- main.js
+++ main.js
@@ -1,14 +1,10 @@
-const ulEl = document.querySelector('ul');
 
-console.log(ulEl);
-
-for (let i=0; i<3; i+=1) {
-  const li = document.createElement('li')
-  li.textContent = `list-${i+1}`
-  if ((i+1)%2 ===0) {
-    li.addEventListener('click',function () {
-      console.log(li.textContent)
-    })
-  }
-  ulEl.appendChild(li)
+function user (first, last){
+  this.firstName = first
+  this.lastName = last 
+  this.fullName = first+last
 }
+
+const myName = new user('Yoon ', 'woojoung')
+
+console.log(myName);
